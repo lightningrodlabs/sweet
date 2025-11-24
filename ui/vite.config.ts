cdceda4c8cbd7e440b2ed13d78bc1343b7eea1d4
Leo <leobensman@gmail.com>
2025-11-24 18:04:09 +0100

    upgrade to holochain 0.6

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), wasm()],
   build: {
-    minify: false
+    minify: false,
+    target: [
+      'chrome89',
+      'firefox89',
+      'safari15',
+      'edge89',
+      'es2022'
+    ]
   },
   server: {
     hmr: {
-        host: 'localhost',
+      host: 'localhost',
     },
     watch: {
-        usePolling: true
+      usePolling: true
     }
   },
 });
